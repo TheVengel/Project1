8527c5da0ca92aa5bfc787dbe8abb680915bd1ec
Valery Lebedev <valery.lebedev@blablacar.com>
2022-03-29 13:01:47 +0400

    Learning some js functions

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
 let name = 'Adam';
-console.log(name); 
+let age = 20;
+let isMarried = false;
+let lastName = undefined;
+let userColor = null;
+
+let person = {
+    name: 'Adam',
+    age: 28,
+}
+
+console.log(person.name);
+
+
+person['age'] = 25;
+console.log(person['age']);
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+
+
+console.log(selectedColors.length);
+
+function sayHi(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+sayHi('Valery', 'Lebedev');
+
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
